427957161da95339cb00e192a88789936fe344ff
Chaos-Potatoe <jio-boltgun666@live.fr>
2018-04-18 14:48:23 +0200

    essai message simple

---
+++ ClientSocket/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientSocket
+{
+    class Program
+    {
+        private static Socket _socket;
+        private static object ipEndPoint;
+        private static string input;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----- socket client ------\n");
+            Console.Write("Adresse IP: ");
+            string adress = Console.ReadLine();
+            Console.Write("Port : ");
+            string port = Console.ReadLine();
+
+            ClientTCP(IPAddress.Parse(adress), int.Parse(port));
+
+        }
+
+        private static void ClientTCP(IPAddress adress, int port)
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEndPoint = new IPEndPoint(adress, port);
+
+            _socket.Connect(ipEndPoint);
+            var thread = new Thread(Listen);
+            thread.Start();
+        }
+
+        private static void Listen()
+        {
+            while (true)
+            {
+                input = Console.ReadLine();
+                _socket.Send(Encoding.UTF8.GetBytes(input));
+            }
+        }
+    }
+}
+
---
+++ ServeurSocket/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServeurSocket
+{
+    class Program
+    {
+        private static Socket _listenerSocket;
+        private static Socket socketReception;
+        private static byte[] buffer;
+        private static int readBytes;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----- socket serveur ------\n");
+            Console.Write("Port : ");
+            string port = Console.ReadLine();
+            ServerTCP(int.Parse(port));
+        }
+
+        private static void ServerTCP(int port)
+        {
+            _listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var address = GetHostIPAddress();
+            IPEndPoint ip = new IPEndPoint(address, port);
+            _listenerSocket.Bind(ip);
+
+            var listenThread = new Thread(ListenThread);
+            listenThread.Start();
+            Console.WriteLine($"Success... Listening TCP: {address}:{port}");
+        }
+
+        private static void ListenThread()
+        {
+            while (true)
+            {
+                _listenerSocket.Listen(0);
+                //Clients.Add(new ClientManager(  ));
+                socketReception = _listenerSocket.Accept();
+                buffer = new byte[socketReception.SendBufferSize];
+                readBytes = socketReception.Receive(buffer);
+                if (readBytes > 0)
+                {
+                    var msg = Encoding.UTF8.GetString(buffer, 0, readBytes);
+                    Console.WriteLine(msg);
+                }
+            }
+        }
+
+        private static IPAddress GetHostIPAddress()
+        {
+
+            return Dns.GetHostAddresses(Dns.GetHostName()).FirstOrDefault(_ => _.AddressFamily == AddressFamily.InterNetwork);
+
+        }
+        
+
+    }
+}
